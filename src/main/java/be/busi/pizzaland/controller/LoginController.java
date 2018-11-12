8ec218d50364ba7fed2ec09823cf2396fdb63c71
Astrid TCHOUTENG <astrid.tchouteng@gmail.com>
2018-11-12 09:11:25 +0100

    merge

--- src/main/java/be/busi/pizzaland/controller/LoginController.java
+++ src/main/java/be/busi/pizzaland/controller/LoginController.java
@@ -1,16 +1,8 @@
 package be.busi.pizzaland.controller;
 
-import be.busi.pizzaland.model.Constants;
 import be.busi.pizzaland.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
