30d7b6d6aca5a7a6447344635849c6c3df74789b
Ekem herve <ekemherve@gmail.com>
2018-11-12 10:08:45 +0100

    panier update

--- src/main/java/be/busi/pizzaland/dataAccess/dao/PizzaDAO.java
+++ src/main/java/be/busi/pizzaland/dataAccess/dao/PizzaDAO.java
@@ -8,7 +8,7 @@ import be.busi.pizzaland.model.Pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import be.busi.pizzaland.dataAccess.Util.ProviderConverter;
+import be.busi.pizzaland.dataAccess.util.ProviderConverter;
 
 import java.util.Set;
 
